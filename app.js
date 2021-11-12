7ad83eaaf5d9b3b4747dea823442060c3cdcb55b
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-11-12 17:48:54 +0700

    ✨ Adding html page

--- app.js
+++ app.js
@@ -7,5 +7,22 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Giffari Alfarizy page</h1>');
+  // res.send('<h1>Giffari Alfarizy page</h1>');
+  res.sendFile('./views/index.html', { root: __dirname });
+});
+
+app.get('/about', (req, res) => {
+  // res.send('<h1>About page</h1>');
+  res.sendFile('./views/about.html', { root: __dirname });
+});
+
+// redirect
+app.get('/about-us', (req, res) => {
+  res.redirect('/about');
+});
+
+// 404 page
+// if it did not find match
+app.use((req, res) => {
+  res.status(404).sendFile('./views/404.html', { root: __dirname });
 });
