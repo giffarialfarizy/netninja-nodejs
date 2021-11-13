7f8c12159e5f157db5ee5eda022d602d2edae73e
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-11-13 11:28:40 +0700

    🗃️ MongoDB and mongoose

--- app.js
+++ app.js
@@ -1,14 +1,83 @@
 const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const Blog = require('./models/blog');
 
 // express app
 const app = express();
 
+// Connect to mongodb
+const dbURI =
+  'mongodb+srv://giffari:DragonFly123@firstcluster.he2uo.mongodb.net/first-database?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  // listen for requests
+  .then((result) => app.listen(3000))
+  // catch error
+  .catch((err) => console.log(err));
+
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+// app.use((req, res, next) => {
+//   console.log('new request made: ');
+//   console.log('host: ', req.hostname);
+//   console.log('path: ', req.path);
+//   console.log('method: ', req.method);
+//   // console.log(req);
+//   next();
+// });
 
+// middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+// mongoose and mongo sandbox routes
+// save new blog
+app.get('/add-blog', (req, res) => {
+  const blog = new Blog({
+    title: 'another new blog',
+    snippet: 'about my new blog',
+    body: 'more about my new blog',
+  });
+
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// find All
+app.get('/all-blogs', (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// find by ID
+app.get('/single-blog', (req, res) => {
+  Blog.findById('618f3b8bfaadbddadfefb9ef')
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// routes
 app.get('/', (req, res) => {
   // res.send('<h1>Giffari Alfarizy page</h1>');
   // res.sendFile('./views/index.html', { root: __dirname });
@@ -19,6 +88,11 @@ app.get('/', (req, res) => {
   ];
   res.render('index', { title: 'Home', blogs });
 });
+
+// app.use((req, res, next) => {
+//   console.log('in the next middleware');
+//   next();
+// });
 
 app.get('/about', (req, res) => {
   // res.send('<h1>About page</h1>');
